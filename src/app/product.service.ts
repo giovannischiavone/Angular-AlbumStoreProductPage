140ee2183d559500dff3ec4da195ceed1852a669
giovannischiavone <giovanni.schiavone@capgemini.com>
2018-10-21 13:16:39 +0200

    test 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product} from './product';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUlr = '../assets/products.json';
+
   constructor(private _http: Http) { }
 
   getAlbum(id: number) : Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => 
     <Album>response.json());
+  }
+
+  getProducts() : Observable<Product[]> {
+    return this._http.get(this._productsUlr).map((response) => 
+    <Product[]>response.json());
   }
 }
